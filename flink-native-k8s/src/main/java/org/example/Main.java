b2c0351d260f3122ac96a67009e08910003f0e9a
dujie <dujie@dtstack.com>
2023-06-01 17:31:17 +0800

    add binance kafkasql

--- flink-native-k8s/src/main/java/org/example/Main.java
+++ flink-native-k8s/src/main/java/org/example/Main.java
@@ -4,8 +4,10 @@ import org.apache.flink.client.deployment.ClusterClientFactory;
 import org.apache.flink.client.deployment.ClusterDescriptor;
 import org.apache.flink.client.program.ClusterClient;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.DeploymentOptions;
 import org.apache.flink.configuration.GlobalConfiguration;
 import org.apache.flink.kubernetes.KubernetesClusterClientFactory;
+import org.apache.flink.kubernetes.executors.KubernetesSessionClusterExecutor;
 
 public class Main {
     public static void main(String[] args)throws Exception {
@@ -16,6 +18,7 @@ public class Main {
         flinkConfigure.setString("taskmanager.numberOfTaskSlots","2");
         flinkConfigure.setString("taskmanager.memory.process.size","2g");
         flinkConfigure.setString("kubernetes.taskmanager.cpu","1");
+        flinkConfigure.set(DeploymentOptions.TARGET, KubernetesSessionClusterExecutor.NAME);
 
         final ClusterClientFactory<String> kubernetesClusterClientFactory =
                 new KubernetesClusterClientFactory();
