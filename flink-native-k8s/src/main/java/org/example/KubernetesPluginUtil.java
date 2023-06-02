e46a64873bcec2da4fd578b9e508b632761ea6a7
dujie <dujie@dtstack.com>
2023-06-02 13:44:06 +0800

    add binance kafkasql

---
+++ flink-native-k8s/src/main/java/org/example/KubernetesPluginUtil.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example;
+
+
+import com.google.common.base.Charsets;
+import io.fabric8.kubernetes.api.model.Quantity;
+import io.fabric8.kubernetes.api.model.ResourceQuota;
+import io.fabric8.kubernetes.api.model.ResourceQuotaList;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.client.deployment.ClusterSpecification;
+import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.PackagedProgram;
+import org.apache.flink.client.program.ProgramInvocationException;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.configuration.CoreOptions;
+import org.apache.flink.configuration.JobManagerOptions;
+import org.apache.flink.configuration.TaskManagerOptions;
+import org.apache.flink.core.execution.DetachedJobExecutionResult;
+import org.apache.flink.kubernetes.configuration.KubernetesConfigOptions;
+import org.apache.flink.runtime.clusterframework.TaskExecutorProcessUtils;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
+import org.apache.flink.runtime.jobmanager.JobManagerProcessUtils;
+import org.apache.flink.util.ExceptionUtils;
+import org.apache.flink.util.JarUtils;
+import org.apache.flink.util.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.misc.URLClassPath;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.apache.flink.util.Preconditions.checkNotNull;
+
+/**
+ * Date: 2021/10/22 Company: www.dtstack.com
+ *
+ * @author tudou
+ */
+public class KubernetesPluginUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(KubernetesPluginUtil.class);
+    public static final String tmpK8sConfigDir = "tmpK8sConf";
+    public static final Pattern EFFECTIVE_TASK_NAME_PATTERN =
+            Pattern.compile("[a-z]([-a-z0-9]*[a-z0-9])?");
+
+
+    public static PackagedProgram buildProgram(
+            String jarPath,
+            List<URL> classPaths,
+            String entryPointClass,
+            String[] programArgs,
+            SavepointRestoreSettings spSetting,
+            Configuration flinkConfiguration)
+            throws ProgramInvocationException {
+        if (jarPath == null) {
+            throw new IllegalArgumentException("The program JAR file was not specified.");
+        }
+        File jarFile = new File(jarPath);
+
+        Configuration flinkConfig =
+                new Configuration(flinkConfiguration);
+
+
+        // 指定采用parent ClassLoader优先加载的类
+        String append =
+                flinkConfig.getString(CoreOptions.ALWAYS_PARENT_FIRST_LOADER_PATTERNS_ADDITIONAL);
+        String dtstackAppend = "com.fasterxml.jackson.";
+        if (StringUtils.isNotEmpty(append)) {
+            dtstackAppend = dtstackAppend + ";" + append;
+        }
+        flinkConfig.setString(
+                CoreOptions.ALWAYS_PARENT_FIRST_LOADER_PATTERNS_ADDITIONAL, dtstackAppend);
+
+        PackagedProgram program =
+                PackagedProgram.newBuilder()
+                        .setJarFile(jarFile)
+                        .setUserClassPaths(classPaths)
+                        .setEntryPointClassName(entryPointClass)
+                        .setConfiguration(flinkConfig)
+                        .setArguments(programArgs)
+                        .setSavepointRestoreSettings(spSetting)
+                        .build();
+
+        return program;
+    }
+}
--- flink-native-k8s/src/main/java/org/example/Main.java
+++ flink-native-k8s/src/main/java/org/example/Main.java
@@ -1,23 +1,33 @@
 package org.example;
 
+import com.google.common.collect.Lists;
 import org.apache.flink.client.deployment.ClusterClientFactory;
 import org.apache.flink.client.deployment.ClusterDescriptor;
 import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.PackagedProgram;
+import org.apache.flink.client.program.PackagedProgramUtils;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.DeploymentOptions;
 import org.apache.flink.configuration.GlobalConfiguration;
+import org.apache.flink.core.execution.DetachedJobExecutionResult;
 import org.apache.flink.kubernetes.KubernetesClusterClientFactory;
 import org.apache.flink.kubernetes.executors.KubernetesSessionClusterExecutor;
+import org.apache.flink.runtime.jobgraph.JobGraph;
+import org.apache.flink.runtime.jobgraph.SavepointRestoreSettings;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         Configuration flinkConfigure = GlobalConfiguration.loadConfiguration("/Users/yh/Downloads/flink-1.12.7/conf");
-        flinkConfigure.setString("kubernetes.cluster-id","flink-k8s-session-cluster-yh");
-        flinkConfigure.setString("kubernetes.jobmanager.service-account","flink");
-        flinkConfigure.setString("kubernetes.rest-service.exposed.type","NodePort");
-        flinkConfigure.setString("taskmanager.numberOfTaskSlots","2");
-        flinkConfigure.setString("taskmanager.memory.process.size","2g");
-        flinkConfigure.setString("kubernetes.taskmanager.cpu","1");
+        flinkConfigure.setString("kubernetes.cluster-id", "flink-k8s-session-cluster-yh");
+        flinkConfigure.setString("kubernetes.jobmanager.service-account", "flink");
+        flinkConfigure.setString("kubernetes.rest-service.exposed.type", "NodePort");
+        flinkConfigure.setString("taskmanager.numberOfTaskSlots", "2");
+        flinkConfigure.setString("taskmanager.memory.process.size", "2g");
+        flinkConfigure.setString("kubernetes.taskmanager.cpu", "1");
         flinkConfigure.set(DeploymentOptions.TARGET, KubernetesSessionClusterExecutor.NAME);
 
         final ClusterClientFactory<String> kubernetesClusterClientFactory =
@@ -31,6 +41,133 @@ public class Main {
                                 flinkConfigure))
                 .getClusterClient();
         String clusterId = clusterClient.getClusterId();
+
+
+        clusterClient.submitJob(buildJobgraph(flinkConfigure))
+                .thenApply(DetachedJobExecutionResult::new)
+                .get(5, TimeUnit.MINUTES);
+
         System.out.println("Hello world!");
     }
+
+    public static JobGraph buildJobgraph(Configuration flinkConfigure) throws Exception {
+        List<String> programArgList = buildFlinkxProgramArgs();
+
+        PackagedProgram packagedProgram =
+                KubernetesPluginUtil.buildProgram(
+                        "/opt/data/chunjun-dist/chunjun-core-release_1.12_5.3.x.jar",//chunjuncore路径
+                        Lists.newArrayList(),//
+                        "com.dtstack.chunjun.Main",
+                        programArgList.toArray(new String[programArgList.size()]),
+                        SavepointRestoreSettings.none(),
+                        flinkConfigure);
+
+        return
+                PackagedProgramUtils.createJobGraph(
+                        packagedProgram,
+                        flinkConfigure,
+                        1,
+                        false);
+
+    }
+
+    public static List<String> buildFlinkxProgramArgs()
+            throws Exception {
+
+        List<String> programArgList = new ArrayList<>(16);
+        //        if (StringUtils.isNotBlank(args)) {
+        //            // 按空格,制表符等进行拆分
+        //            programArgList.addAll(Arrays.asList(args.split("\\s+")));
+        //        }
+        programArgList.add("-mode");
+        String mode = "kubernetes-session";
+        programArgList.add(mode);
+        programArgList.add("-jobType");
+        programArgList.add("sync");
+        programArgList.add("-job");
+
+        programArgList.add(getsync());
+        programArgList.add("-jobName");
+        programArgList.add("test");
+        programArgList.add("-pluginLoadMode");
+        programArgList.add("shipfile");
+        programArgList.add("-flinkxDistDir");
+        programArgList.add("/opt/data/chunjun-dist");
+
+
+        return programArgList;
+    }
+
+    public static String getsync() {
+        return "{\n" +
+                "  \"job\": {\n" +
+                "    \"content\": [\n" +
+                "      {\n" +
+                "        \"reader\": {\n" +
+                "          \"parameter\": {\n" +
+                "            \"column\": [\n" +
+                "              {\n" +
+                "                \"name\": \"id\",\n" +
+                "                \"type\": \"id\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"name\",\n" +
+                "                \"type\": \"string\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"content\",\n" +
+                "                \"type\": \"string\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"sliceRecordCount\": [\"30\"],\n" +
+                "            \"permitsPerSecond\": 1\n" +
+                "          },\n" +
+                "          \"table\": {\n" +
+                "            \"tableName\": \"sourceTable\"\n" +
+                "          },\n" +
+                "          \"name\": \"streamreader\"\n" +
+                "        },\n" +
+                "        \"writer\": {\n" +
+                "          \"parameter\": {\n" +
+                "            \"column\": [\n" +
+                "              {\n" +
+                "                \"name\": \"id\",\n" +
+                "                \"type\": \"id\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"name\",\n" +
+                "                \"type\": \"string\"\n" +
+                "              },\n" +
+                "              {\n" +
+                "                \"name\": \"content\",\n" +
+                "                \"type\": \"timestamp\"\n" +
+                "              }\n" +
+                "            ],\n" +
+                "            \"print\": true\n" +
+                "          },\n" +
+                "          \"table\": {\n" +
+                "            \"tableName\": \"sinkTable\"\n" +
+                "          },\n" +
+                "          \"name\": \"streamwriter\"\n" +
+                "        },\n" +
+                "        \"transformer\": {\n" +
+                "          \"transformSql\": \"select id,name, NOW() from sourceTable where CHAR_LENGTH(name) < 50 and CHAR_LENGTH(content) < 50\"\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"setting\": {\n" +
+                "      \"errorLimit\": {\n" +
+                "        \"record\": 100\n" +
+                "      },\n" +
+                "      \"speed\": {\n" +
+                "        \"bytes\": 0,\n" +
+                "        \"channel\": 1,\n" +
+                "        \"readerChannel\": 1,\n" +
+                "        \"writerChannel\": 1\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}\n";
+    }
+
 }
